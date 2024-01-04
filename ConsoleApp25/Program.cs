1dc73516fe8df167c8a80cfc64b9ab9c8a71dd0d
sulton <global-sultonmalik326@gmail.com>
2024-01-04 15:03:51 +0500

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Fayl nomini kiriting: ");
+        string filename = Console.ReadLine();
+
+        try
+        {
+            string fullPath = FindFile(filename);
+
+            if (!string.IsNullOrEmpty(fullPath))
+            {
+                Console.WriteLine($"Fayl topildi. Fayl joylashgan path: {fullPath}");
+            }
+            else
+            {
+                Console.WriteLine("Fayl topilmadi.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Xatolik yuz berdi: {ex.Message}");
+        }
+    }
+
+    static string FindFile(string filename)
+    {
+     
+        string[] drives = Directory.GetLogicalDrives();
+
+        foreach (var drive in drives)
+        {
+            string fullPath = Path.Combine(drive, filename);
+
+            if (File.Exists(fullPath))
+            {
+                return fullPath;
+            }
+        }
+
+        return null;
+    }
+}
